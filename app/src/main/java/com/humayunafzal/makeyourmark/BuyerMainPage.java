a480e5de162ec0afb65c48c9ea22bb9f6b9afd38
halts440 <52453910+halts440@users.noreply.github.com>
2021-10-19 21:29:37 +0500

    Implemented Designs

---
+++ app/src/main/java/com/humayunafzal/makeyourmark/BuyerMainPage.java
@@ -0,0 +1,14 @@
+package com.humayunafzal.makeyourmark;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BuyerMainPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buyer_main_page);
+    }
+}
---
+++ app/src/main/java/com/humayunafzal/makeyourmark/SellerAllProducts.java
@@ -0,0 +1,14 @@
+package com.humayunafzal.makeyourmark;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SellerAllProducts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seller_all_products);
+    }
+}
